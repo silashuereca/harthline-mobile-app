2cfcbc140882997cc0d8147f827f23c333a4bb73
Silas Huereca <silashuereca.dev@gmail.com>
2025-08-25 15:15:59 -0500

    feat: ability to run in IOS simulator xcode

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 /// <reference types="vitest" />
-
 import legacy from "@vitejs/plugin-legacy";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), legacy()],
@@ -12,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
