62786fe01fb91c072da7ad22d2bea6ba12b6d5b8
Silas Huereca <silashuereca.dev@gmail.com>
2025-08-25 19:29:57 -0500

    feat: add in supabase client function helpers

---
+++ src/supabase.ts
@@ -0,0 +1,16 @@
+import { createClient, User } from "@supabase/supabase-js";
+
+import { Database } from "../supabase/functions/_shared/database-types";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export type TAuthUser = User;
+
+export async function getUser(): Promise<User> {
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    throw new Error(`Failed to get user: ${error.message}`);
+  }
+  return data.user;
+}
